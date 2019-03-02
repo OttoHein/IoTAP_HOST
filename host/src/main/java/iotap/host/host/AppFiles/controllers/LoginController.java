8ce55629a633f98890c705a53035ed733ec084e1
fflp.felix@gmail.com <t3lg0nr1r3>
2019-03-02 10:18:03 +0100

    inlog employee

--- host/src/main/java/iotap/host/host/AppFiles/controllers/LoginController.java
+++ host/src/main/java/iotap/host/host/AppFiles/controllers/LoginController.java
@@ -1,24 +1,31 @@
-package iotap.host.host.controllers;
+package iotap.host.host.AppFiles.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping("/employee")
 public class LoginController {
 
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
-    @RequestMapping(value={"/home"}, method = RequestMethod.GET)
-    public ModelAndView course(){
+    @RequestMapping(value={"/scan/{id}"}, method = RequestMethod.GET)
+    public String employerScan(@PathVariable("id") String id){
+        return "redirect:index/" + id;
+    }
+
+    @RequestMapping(value={"/index/{id}"}, method = RequestMethod.GET)
+    public ModelAndView employee(@PathVariable("id") String id){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
+        modelAndView.addObject("id", id);
+        modelAndView.setViewName("employeeIndex");
         return modelAndView;
-        
     }
 }
