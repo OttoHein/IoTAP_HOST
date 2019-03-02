52fafcff073bf303162d5ce00d21850c9f7158a9
fflp.felix@gmail.com <t3lg0nr1r3>
2019-03-02 13:04:43 +0100

    Pictures

--- host/src/main/java/iotap/host/host/AppFiles/controllers/LoginController.java
+++ host/src/main/java/iotap/host/host/AppFiles/controllers/LoginController.java
@@ -1,27 +1,26 @@
 package iotap.host.host.AppFiles.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
-@RequestMapping("/employee")
 public class LoginController {
 
-    @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/employee/test"}, method = RequestMethod.GET)
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
-    @RequestMapping(value={"/scan/{id}"}, method = RequestMethod.GET)
-    public String employerScan(@PathVariable("id") String id){
-        return "redirect:index/" + id;
+    @RequestMapping(value={"/employee/scan/{id}"}, method = RequestMethod.GET)
+    public RedirectView employerScan(@PathVariable("id") String id){
+        return new RedirectView("/employee/index/" + id);
     }
 
-    @RequestMapping(value={"/index/{id}"}, method = RequestMethod.GET)
+    @RequestMapping(value={"/employee/index/{id}"}, method = RequestMethod.GET)
     public ModelAndView employee(@PathVariable("id") String id){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("id", id);
