9f1c479ad34b7fce8a6c845036ac214c51a634c4
fflp.felix@gmail.com <t3lg0nr1r3>
2019-03-02 14:16:09 +0100

    Save

--- host/src/main/java/iotap/host/host/AppFiles/controllers/EmployeeController.java
+++ host/src/main/java/iotap/host/host/AppFiles/controllers/EmployeeController.java
@@ -1,7 +1,9 @@
 package iotap.host.host.AppFiles.controllers;
 
 import iotap.host.host.AppFiles.entities.Employee;
+import iotap.host.host.AppFiles.entities.Task;
 import iotap.host.host.AppFiles.repositories.EmployeeRepository;
+import iotap.host.host.AppFiles.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.List;
+
 @RestController
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
 
     @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
     public String index() {
@@ -39,9 +46,10 @@ public class EmployeeController {
     @RequestMapping(value={"employee/tasks/{id}"}, method = RequestMethod.GET)
     public ModelAndView employeeTasks(@PathVariable("id") Integer id){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("id", id);
-        modelAndView.setViewName("employeeIndex");
+        List<Task> tasks = taskRepository.findAll();
+        modelAndView.addObject("tasks", tasks);
+        modelAndView.setViewName("employeeTasks");
         return modelAndView;
     }
-    
+
 }
